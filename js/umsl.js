8ed183c76efa9d2c798d6ff1873c3f472aeede0e
Phil Redmon <redmonp@missouri.edu>
2015-10-21 09:11:31 -0500

    Update css and js

--- js/umsl.js
+++ js/umsl.js
@@ -20,10 +20,10 @@ jQuery (document).ready(function() {
 
 function newBrandFunction () {
 var modsOwner = document.getElementById("mods-owner").textContent;
-if  (modsOwner.indexOf("Missouri")>=0) { jQuery("#block-block-25").append("<div class='banner'><h1>St. Louis Mercantile Library</h1></div>"); }
-else if  (modsOwner.indexOf("Pott")>=0) { jQuery("#block-block-25").append("<div class='banner'><h1>Herman T. Pott National Inland Waterways Library</h1></div>"); }
-else if  (modsOwner.indexOf("Barriger")>=0) { jQuery("#block-block-25").append("<div class='banner'><h1>John W. Barriger III National Railroad Library</h1></div>"); }
-else if  (modsOwner.indexOf("Art")>=0) { jQuery("#block-block-25").append("<div class='banner'><h1>St. Louis Mercantile Library Art Museum</h1></div>");}
+if  (modsOwner.indexOf("Missouri")>=0) { jQuery("#block-block-25").append("<div class='banner'><a href='http://dl.mospace.umsystem.edu/umsl/islandora/object/umsl%3A2632'><h1>St. Louis Mercantile Library</h1></a></div>"); }
+else if  (modsOwner.indexOf("Pott")>=0) { jQuery("#block-block-25").append("<div class='banner'><a href='http://dl.mospace.umsystem.edu/umsl/islandora/object/umsl%3A2633'><h1>Herman T. Pott National Inland Waterways Library</h1></a></div>"); }
+else if  (modsOwner.indexOf("Barriger")>=0) { jQuery("#block-block-25").append("<div class='banner'><a href='http://dl.mospace.umsystem.edu/umsl/islandora/object/umsl%3A2634'><h1>John W. Barriger III National Railroad Library</h1></a></div>"); }
+else if  (modsOwner.indexOf("Art")>=0) { jQuery("#block-block-25").append("<div class='banner'><a href='http://dl.mospace.umsystem.edu/umsl/islandora/object/umsl%3A109460'><h1>St. Louis Mercantile Library Art Museum</h1></a></div>");}
 else if  (modsOwner.indexOf("Jefferson")>=0) { jQuery("#block-block-25").append("<div class='banner'><h1>Thomas Jefferson Library</h1></div>"); }
 else if  (modsOwner.indexOf("Archives")>=0) { jQuery("#block-block-25").append("<div class='banner'><h1>University Archives</h1></div>"); }
 else if  (modsOwner.indexOf("UMSL")>=0) { 
